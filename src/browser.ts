fae57e16972377f3e350eeb16a737768182d1476
fgafford <forest.gafford@gmail.com>
2017-05-25 23:24:20 -0400

    Basic React render working

---
+++ src/browser.ts
@@ -0,0 +1,5 @@
+/**
+ * All globaly accessible brower loaded libraries should be decliared
+ * here to immulate the browser enviroment for TypeScript compiler
+ */
+/// <reference path="../node_modules/@types/react/index.d.ts" />
